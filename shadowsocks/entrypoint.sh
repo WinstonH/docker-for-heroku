6883c0eb1f293ea411964f4a053c09bacbb3464e
Winston <bjdhjj@gmail.com>
2018-10-21 17:45:57 +0800

    add ss

---
+++ shadowsocks/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ -n "$PASSWORD" ];then
+    sed -i "s/herosocks/$PASSWORD/g" /etc/v2ray/config.json
+    echo "You've set a password"
+    echo "The password is: $PASSWORD"
+else
+    echo "You didn't set a password"
+    echo "Use default password: herosocks"
+fi
+sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
+mkdir -p /var/tmp/nginx/client_body
+mkdir /run/nginx
+# start nginx
+nginx
+
+# ss
+ss-server -c /root/config.json
