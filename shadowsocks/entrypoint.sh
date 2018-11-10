c41370ac7b5031c1c1f2ea31ae0238b136c43c4f
Winston <bjdhjj@gmail.com>
2018-11-10 14:19:42 +0800

    fix ss

--- shadowsocks/entrypoint.sh
+++ shadowsocks/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -n "$PASSWORD" ];then
-    sed -i "s/herosocks/$PASSWORD/g" /etc/v2ray/config.json
+    sed -i "s/herosocks/$PASSWORD/g" /root/config.json
     echo "You've set a password"
     echo "The password is: $PASSWORD"
 else
